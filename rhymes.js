e912b812c2ce90f5116e9efa488930906ad2ebbf
pham2jt <93550337+pham2jt@users.noreply.github.com>
2022-11-07 19:55:19 -0500

    works now

--- rhymes.js
+++ rhymes.js
@@ -12,22 +12,48 @@ const results = document.getElementById('results');
 
 // add event listener to know when to search
 
-queryInputElem.addEventListener('keyup', async function(ev) {
+queryInputElem.addEventListener('keyup', async function (ev) {
   ev.preventDefault()
   if (ev.key == 'Enter') {
-    console.log('pressed enter')
-
-    const rhymeResultsResp = await fetch(
-      `https://rhymebrain.com/talk?function=getRhymes&word=${queryInputElem.value}`);
-      console.log(rhymeResultsResp);
-    const rhymeResults = await rhymeResultsResp.json();
-
-    console.log(rhymeResults);
-
-  }
+    console.log('pressed enter');
+    // const rhymeResultsResp = await fetch(
+    //   `https://rhymebrain.com/talk?function=getRhymes&word=${queryInputElem.value}`);
+    // console.log(rhymeResultsResp);
+    
+    // fetch(rhymeResultsResp)
+    //   .then((results) => results.json())
+    //   .then(
+    //       results.forEach((result) => {
+    //       result.style = parseInt(result.dataset.score, 10);
+    //       result.style.fontSize = '$(.5 + (3.5 * resultScore / 300}.rem';
+    //       resultElem.classList.add(result);
+    //     })
+    //   )
+    //   .catch((error) => {
+    //     console.error(`there was an error ${error}`);
+    //   });
+    // }
+    fetch(`https://rhymebrain.com/talk?function=getRhymes&word=${queryInputElem.value}`)
+      .then((results) => results.json())
+      .then((result) => {
+        const div = document.getElementById('results');
+        result.forEach((word) => {
+            const resultScore = parseInt(word.score, 10);
+            const child = div.appendChild(document.createElement('span'));
+            child.innerText = word.word + ' ';
+            child.style.fontSize = `${0.5 + (3.5 * resultScore) / 300}rem`;
+          }
+        )
+        
+      })
+      .catch(console.error);
+      
+    
+  };
 });
 
 // write function that searches the rhyme API given a (string) query (likely you should use the fetch API)
+
 
 // write function that:
 //  1. expects array of word object results 
