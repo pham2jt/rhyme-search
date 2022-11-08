eba4eec162e2d1e0256d1f64766f74c0c9de24c1
pham2jt <93550337+pham2jt@users.noreply.github.com>
2022-11-07 20:36:22 -0500

    part 2 done

--- rhymes.js
+++ rhymes.js
@@ -12,50 +12,47 @@ const results = document.getElementById('results');
 
 // add event listener to know when to search
 
+let count = 0;
+
 queryInputElem.addEventListener('keyup', async function (ev) {
   ev.preventDefault()
   if (ev.key == 'Enter') {
     console.log('pressed enter');
-    // const rhymeResultsResp = await fetch(
-    //   `https://rhymebrain.com/talk?function=getRhymes&word=${queryInputElem.value}`);
-    // console.log(rhymeResultsResp);
-    
-    // fetch(rhymeResultsResp)
-    //   .then((results) => results.json())
-    //   .then(
-    //       results.forEach((result) => {
-    //       result.style = parseInt(result.dataset.score, 10);
-    //       result.style.fontSize = '$(.5 + (3.5 * resultScore / 300}.rem';
-    //       resultElem.classList.add(result);
-    //     })
-    //   )
-    //   .catch((error) => {
-    //     console.error(`there was an error ${error}`);
-    //   });
-    // }
     fetch(`https://rhymebrain.com/talk?function=getRhymes&word=${queryInputElem.value}`)
       .then((results) => results.json())
       .then((result) => {
         const div = document.getElementById('results');
         result.forEach((word) => {
-            const resultScore = parseInt(word.score, 10);
-            const child = div.appendChild(document.createElement('span'));
-            child.innerText = word.word + ' ';
-            child.style.fontSize = `${0.5 + (3.5 * resultScore) / 300}rem`;
+          const resultScore = parseInt(word.score, 10);
+          const child = div.appendChild(document.createElement('span'));
+          child.innerText = word.word + ' ';
+          child.style.fontSize = `${0.5 + (3.5 * resultScore) / 300}rem`;
+          count += 1;
+          if (count <= 10) {
+            dictionary(word.word, child);
           }
+        }
         )
         
       })
       .catch(console.error);
-      
-    
   };
 });
 
-// write function that searches the rhyme API given a (string) query (likely you should use the fetch API)
-
-
-// write function that:
-//  1. expects array of word object results 
-//    that look like the spec says https://rhymebrain.com/api.html#rhyme
-//  2. creates DOM elements and inserts them into the page
+async function dictionary(word, child) {
+  fetch(`https://rhymebrain.com/talk?function=getWordInfo&word=${word}`)
+    .then((results) => results.json())
+    .then((result) => {
+      const keys = Object.keys(result);
+      for (const key of keys) {
+        const list = child.appendChild(document.createElement('dl'));
+        const title = list.appendChild(document.createElement('dt'));
+        title.innerText = key + ':'
+        title.style.fontSize = '20px'
+        const stuff = list.appendChild(document.createElement('dd'));
+        stuff.innerText = result[key]
+        stuff.style.fontSize = '12px'
+      }
+    })
+  
+}
